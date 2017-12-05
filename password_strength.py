a626c9d94d37fad983099067d76e9995921cc3e1
4pydev <4pydev@gmail.com>
2017-12-05 15:19:22 +0300

    Added argparse and fixed remarks.

--- password_strength.py
+++ password_strength.py
@@ -1,25 +1,27 @@
+import argparse
 from getpass import getpass
 
 
-PASS_FEATURES_WEIGHTS = {
-    'has_case_sensitivity': 1,
-    'has_digits': 2,
-    'has_special_chars': 2,
-    'not_in_blacklist': 4
-}
-BLACKLIST = './blacklist.txt'
-
-
-def load_blacklist(blacklist_file=BLACKLIST):
+def load_blacklist(blacklist_file):
     try:
         with open(blacklist_file, 'r') as file:
-            black_list = file.read().split()
-        return black_list
+            return [bad_pass.strip() for bad_pass in file.readlines()]
     except FileNotFoundError:
+        print("Specified blacklist file and/or \n"
+              "default blacklist file doesn't exist.\n"
+              "Blacklist test wasn't be performed.")
         return []
 
 
-def get_password_strength(password):
+def get_password_strength(password, black_list):
+
+    PASS_FEATURES_WEIGHTS = {
+        'has_case_sensitivity': 1,
+        'has_digits': 2,
+        'has_special_chars': 2,
+        'not_in_blacklist': 4
+    }
+
     # initial password strength
     password_strength = 1
 
@@ -32,35 +34,26 @@ def get_password_strength(password):
     if has_special_chars(password):
         password_strength += PASS_FEATURES_WEIGHTS['has_special_chars']
 
-    if not is_in_blacklist(password):
+    if not is_in_blacklist(password, black_list):
         password_strength += PASS_FEATURES_WEIGHTS['not_in_blacklist']
+
     return password_strength
 
 
 def has_case_sensitivity(password):
-    upper_chars = 0
-    lower_chars = 0
-    for char in password:
-        upper_chars += 1 if char.isupper() else False
-        lower_chars += 1 if char.islower() else False
-    return bool(upper_chars > 0 and lower_chars > 0)
+    return True if any(char.islower() for char in password) and \
+                   any(char.isupper() for char in password) else False
 
 
 def has_digits(password):
-    for char in password:
-        if char.isdigit():
-            return True
-    return False
+    return True if any(char.isdigit() for char in password) else False
 
 
 def has_special_chars(password):
-    for char in password:
-        if not char.isalpha() and not char.isdigit():
-            return True
-    return False
+    return True if not password.isalnum() else False
 
 
-def is_in_blacklist(password, black_list=load_blacklist(BLACKLIST)):
+def is_in_blacklist(password, black_list):
     if black_list == []:
         return True
     else:
@@ -71,6 +64,12 @@ def is_in_blacklist(password, black_list=load_blacklist(BLACKLIST)):
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--blacklist', default='./blacklist.txt')
+    args = parser.parse_args()
+
     usr_password = getpass("Enter a password: ")
-    print("Your password strength: {}".format(
-                        get_password_strength(usr_password)))
+    print("Your password's strength: {}".format(
+                    get_password_strength(usr_password,
+                                          load_blacklist(args.blacklist))))
